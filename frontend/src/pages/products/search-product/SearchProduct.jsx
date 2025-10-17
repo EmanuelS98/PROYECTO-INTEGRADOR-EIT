68563b5d2278fef5ddb883271272c9298defd29b
Emanuel Solari <emanuelsolarimdq@gmail.com>
2025-10-16 21:10:13 -0300

    fix dependency

---
+++ frontend/src/pages/products/search-product/SearchProduct.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import "./search-product.scss";
+import { Button, TextField } from "@mui/material";
+
+const SearchProduct = ({ entryValue, onChange, onClear }) => {
+    return (
+        <div className="search-product">
+            <TextField
+                className="search-input"
+                placeholder="Buscar producto..."
+                onChange={onChange}
+                value={entryValue}/>
+
+            <Button className="clear-button" variant="contained" onClick={onClear}>
+                Limpiar
+            </Button>
+        </div>
+    );
+};
+
+SearchProduct.propTypes = {
+    entryValue: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onClear: PropTypes.func.isRequired,
+};
+
+export default SearchProduct;
