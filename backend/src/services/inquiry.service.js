44aabac82e1675f59ec411b7b19fe36e470d0b95
Emanuel Solari <emanuelsolarimdq@gmail.com>
2025-10-24 23:15:37 -0300

    Mejoras visuales

--- backend/src/services/inquiry.service.js
+++ backend/src/services/inquiry.service.js
@@ -1,31 +1,47 @@
-import { sendMail } from "../utils/mailer.js";
-import ErrorService from "./error.service.js";
+import SibApiV3Sdk from "sib-api-v3-sdk";
 
 export default class InquiryService {
+    #apiInstance;
+
+    constructor() {
+        const defaultClient = SibApiV3Sdk.ApiClient.instance;
+        const apiKey = defaultClient.authentications["api-key"];
+        apiKey.apiKey = process.env.BREVO_API_KEY;
+        this.#apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    }
+
     async sendInquiry(data) {
         const { name, surname, phone, email, inquiry } = data;
 
-        const from = `"${name} ${surname}" <${process.env.SMTP_FROM}>`;
-        const to = process.env.SMTP_RECIPIENT;
-        const subject = "Consulta Web";
-        const contentHtml = `
-        <div>
-        <div style="font-family: Arial, sans-serif; padding:20px;">
-            <h2 style="color: #333;">Nueva Consulta desde el sitio Web</h2>
-            <p>Nombre: ${name}</p>
-            <p>Apellido: ${surname}</p>
-            <p>Teléfono: ${phone}</p>
-            <p>Email: ${email}</p>
-            <p>Consulta: </p>
-            <p style="background-color: #f5f5f5; padding: 10px; border-radius: 5px;">${inquiry}</p>
-        </div>`;
+        const sendSmtpEmail = {
+            sender: {
+                email: process.env.SMTP_FROM,
+                name: `${name} ${surname}`,
+            },
+            to: [{
+                email: process.env.SMTP_RECIPIENT,
+                name: "Administrador",
+            }],
+            subject: "Consulta Web",
+            htmlContent: `
+                <div style="font-family: Arial, sans-serif; padding:20px;">
+                    <h2 style="color: #333;">Nueva Consulta desde el sitio Web</h2>
+                    <p>Nombre: ${name}</p>
+                    <p>Apellido: ${surname}</p>
+                    <p>Teléfono: ${phone}</p>
+                    <p>Email: ${email}</p>
+                    <p>Consulta:</p>
+                    <p style="background-color: #f5f5f5; padding: 10px; border-radius: 5px;">${inquiry}</p>
+                </div>
+            `,
+        };
 
-        const result = await sendMail(from, to, subject, contentHtml);
-
-        if (!result) {
-            throw new ErrorService("Error al enviar el correo");
+        try {
+            const result = await this.#apiInstance.sendTransacEmail(sendSmtpEmail);
+            return result;
+        } catch (error) {
+            console.error("Error sending email:", error);
+            throw new Error("Error al enviar el correo");
         }
-
-        return result;
     }
 }
